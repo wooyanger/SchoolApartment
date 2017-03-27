d5f383f6e5aff772dc22dc1787a44d6e3770105f
wooyang <819844849@qq.com>
2017-03-27 14:47:10 +0800

    Add Notes

--- SchoolApartment.py
+++ SchoolApartment.py
@@ -16,17 +16,24 @@ class SchoolApatment:
         self.session = Session()
         self.m = Mongo('120.25.95.236', 27017, 'schoolapartment')
 
+    # 大学信息
     def university(self):
+        # 获取数据
         data = self.session.get('https://www.wooyang.ml/data/allunivlist.txt').content
+        # 将str 转 list
         data = eval(data)
         for i in data:
+            # 将数据存到对应collection 中
             self.m.insert(collection='country',data={'id': i['id'], 'country_name': i['name']})
             for j in i['univs']:
                 self.m.insert(collection='university', data={'id': j['id'], 'university_name': j['name']})
 
+    # 学院信息
     def college(self):
+        # 从数据库中，取出需要的数据
         for university in self.m.find(collection='university'):
             college_list = []
+            # 获取数据
             data = self.session.get(url='http://www.renren.com/GetDep.do?id=%d' % university['id'] )
             soup = BeautifulSoup(data.text, 'lxml')
             for j in soup.stripped_strings:
@@ -36,7 +43,9 @@ class SchoolApatment:
                     college_list.append(j)
             self.m.insert(collection='college', data={'id': university['id'], 'college': college_list})
 
+    # 宿舍信息
     def apartment(self):
+        # 设置Cookie
         self.session.headers.update({'Cookie': 't=0e10866787e0a7fc1ac190a4d60c4f9e2'})
         for university in self.m.find(collection='university'):
             apartment_list = []
